596e57b97fa24d1be091197d436410e290435517
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-10-26 16:30:38 +0100

    sass

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <h1>hello</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
